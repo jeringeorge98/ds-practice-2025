4b0c36e41f1845b6d42daa821d430f48d0f49b4b
jeringeorge98 <jerringeorge439@gmail.com>
2025-02-21 09:52:42 +0200

    commit

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -4,22 +4,26 @@ import os
 # This set of lines are needed to import the gRPC stubs.
 # The path of the stubs is relative to the current file, or absolute inside the container.
 # Change these lines only if strictly needed.
-FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
-fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/fraud_detection'))
+FILE = __file__ if "__file__" in globals() else os.getenv("PYTHONFILE", "")
+fraud_detection_grpc_path = os.path.abspath(
+    os.path.join(FILE, "../../../utils/pb/fraud_detection")
+)
 sys.path.insert(0, fraud_detection_grpc_path)
 import fraud_detection_pb2 as fraud_detection
 import fraud_detection_pb2_grpc as fraud_detection_grpc
 
 import grpc
 
-def greet(name='you'):
+
+def greet(name="you"):
     # Establish a connection with the fraud-detection gRPC service.
-    with grpc.insecure_channel('fraud_detection:50051') as channel:
+    with grpc.insecure_channel("fraud_detection:50051") as channel:
         # Create a stub object.
         stub = fraud_detection_grpc.HelloServiceStub(channel)
         # Call the service through the stub object.
         response = stub.SayHello(fraud_detection.HelloRequest(name=name))
     return response.greeting
+
 
 # Import Flask.
 # Flask is a web framework for Python.
@@ -32,20 +36,22 @@ import json
 # Create a simple Flask app.
 app = Flask(__name__)
 # Enable CORS for the app.
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 # Define a GET endpoint.
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     """
     Responds with 'Hello, [name]' when a GET request is made to '/' endpoint.
     """
     # Test the fraud-detection gRPC service.
-    response = greet(name='orchestrator')
+    response = greet(name="Jerin")
     # Return the response.
     return response
 
-@app.route('/checkout', methods=['POST'])
+
+@app.route("/checkout", methods=["POST"])
 def checkout():
     """
     Responds with a JSON object containing the order ID, status, and suggested books.
@@ -53,23 +59,23 @@ def checkout():
     # Get request object data to json
     request_data = json.loads(request.data)
     # Print request object data
-    print("Request Data:", request_data.get('items'))
+    print("Request Data:", request_data.get("items"))
 
     # Dummy response following the provided YAML specification for the bookstore
     order_status_response = {
-        'orderId': '12345',
-        'status': 'Order Approved',
-        'suggestedBooks': [
-            {'bookId': '123', 'title': 'The Best Book', 'author': 'Author 1'},
-            {'bookId': '456', 'title': 'The Second Best Book', 'author': 'Author 2'}
-        ]
+        "orderId": "12345",
+        "status": "Order Approved",
+        "suggestedBooks": [
+            {"bookId": "123", "title": "The Best Book", "author": "Author 1"},
+            {"bookId": "456", "title": "The Second Best Book", "author": "Author 2"},
+        ],
     }
 
     return order_status_response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run the app in debug mode to enable hot reloading.
     # This is useful for development.
     # The default port is 5000.
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
